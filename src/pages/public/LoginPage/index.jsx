b6c05522006a630c5321d052b4f8533b08e6caf7
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-02 10:32:27 +0600

    持久化redux方案, 页面首次加载执行任务

--- src/pages/public/LoginPage/index.jsx
+++ src/pages/public/LoginPage/index.jsx
@@ -1,6 +1,32 @@
 import React, { Component } from "react";
 
 export default class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+
+    // do something when open page
+    console.log("hi");
+  }
+
+  componentDidMount() {
+    window.addEventListener(
+      "beforeunload",
+      this.beforeunload
+    );
+  }
+  componentWillUnmount() {
+    window.removeEventListener(
+      "beforeunload",
+      this.beforeunload
+    );
+  }
+
+  beforeunload(e) {
+    e.preventDefault();
+
+    // do something before unload
+  }
+
   render() {
     return <div>Login Page</div>;
   }
