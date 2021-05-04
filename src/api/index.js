7b85d5b9fb197783a56c2dc9e51d588298e072bb
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-04 20:38:36 +0600

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,16 @@
 import { get } from "@api/ajax";
 
 export const testAjax = () => get("/test-api");
+
+
+// 用法:
+// 1.
+//  sendGetRequest = async () => {
+//  const responseData = await testAjax();
+//  console.log(responseData);
+// 
+//  console.log("request success")
+// 
+// 2.
+//  sendGetRequest = () => {
+//  testAjax().then(res => { console.log(res); }
