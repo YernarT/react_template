0b6140b159c9fe65c6bb6e0d8fbbdb49cd0f5775
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-04 20:33:03 +0600

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,3 @@
-import ajax from "@api/ajax";
+import { get } from "@api/ajax";
 
-export const reqLoginWithPhone = (data) => ajax("auth/login/", data, "POST");
-export const reqLoginWithEmail = (data) => ajax("auth/login/", data, "POST");
-
-export const reqRegister = (data) => ajax("auth/registration/", data, "POST");
-
-// Calendar
-export const reqStudentCalendar = (params, jwt) =>
-  ajax("lesson/calendar/", params, "GET", jwt);
-
-// Student Task Content
-export const reqStudentTaskContent = (id, jwt) =>
-  ajax(`lesson/task/${id}/`, {}, "GET", jwt);
+export const testAjax = () => get("/test-api");
