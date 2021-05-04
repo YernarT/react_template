011cdf0492a381037ad566a27a6176ab93bcc36f
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-04 20:32:47 +0600

    Update ajax.js

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -1,24 +1,70 @@
 import axios from "axios";
+import { message } from "antd";
 
-export default function ajax(url, data = {}, method = "GET", jwt = null) {
-  const BASE_URL = "/api/";
+import reduxStore from "@redux/store";
+import { changeFullScreenLoadingAction } from "@redux/actions";
 
-  if (method === "GET") {
-    let paramStr = "";
-    Object.keys(data).forEach((key) => {
-      paramStr += key + "=" + data[key] + "&";
-    });
 
-    if (paramStr) paramStr = paramStr.slice(0, paramStr.length - 1);
+const dataApiInstance = axios.create({
+  baseURL: "/data-api",
+});
 
-    return axios.get(BASE_URL + url + "?" + paramStr, {
-      headers: { Authorization: jwt ? "Bearer " + jwt : "" },
-    });
+dataApiInstance.interceptors.request.use((config) => {
+  const state = reduxStore.getState();
+
+  reduxStore.dispatch(changeFullScreenLoadingAction(true));
+  config.headers["Authorization"] =
+    "Bearer " + state.user.jwt;
+
+  return config;
+});
+
+dataApiInstance.interceptors.response.use(
+  (res) => {
+    reduxStore.dispatch(
+      changeFullScreenLoadingAction(false)
+    );
+
+    return res;
+  },
+  (err) => {
+    reduxStore.dispatch(
+      changeFullScreenLoadingAction(false)
+    );
+
+    if (axios.isCancel(err)) {
+      console.log("请求取消的错误");
+      // 中断promise链
+      return new Promise(() => {});
+    } else {
+      console.log("请求错误");
+      message.error("Error");
+
+      // return Promise.reject(err);
+      // 中断promise链
+      return new Promise(() => {});
+    }
   }
+);
 
-  if (method === "POST") {
-    return axios.post(BASE_URL + url, data, {
-      headers: { Authorization: jwt ? "Bearer " + jwt : "" },
-    });
-  }
+export function get(url, params) {
+  return dataApiInstance.get(url, {
+    params,
+  });
+}
+
+export function post(url, data) {
+  return dataApiInstance.post(url, {
+    data,
+  });
+}
+
+export function put(url, data) {
+  return dataApiInstance.get(url, {
+    data,
+  });
+}
+
+export function del(url) {
+  return dataApiInstance.delete(url);
 }
