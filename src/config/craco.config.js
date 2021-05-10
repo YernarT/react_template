64851e80d402827c4fdab4579895fb39c1dfba2a
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-10 18:55:18 +0600

    Update craco.config.js

--- src/config/craco.config.js
+++ src/config/craco.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   devServer: {
-    port: 3030,
+    port: 3063,
     disableHostCheck: true,
     proxy: {
       "/data-api": {
@@ -36,19 +36,9 @@ module.exports = {
         lessLoaderOptions: {
           lessOptions: {
             modifyVars: {
-              "@primary-color": "#E9012B",
-              "@link-color": "#656678",
-              "@success-color": "#52c41a",
-              "@warning-color": "#faad14",
-              "@error-color": "#f5222d",
-              "@font-size-base": "10px;",
+              "@primary-color": "rgb(17, 236, 229)",
               "@text-color": "#000",
-              "@text-color-secondary": "#9A9CB7",
-              "@disabled-color": "rgba(0, 0, 0, 0.25)",
-              "@border-radius-base": "12px;",
-              "@border-color-base": "#d9d9d9",
-              "@box-shadow-base":
-                "0 2px,2px -2px rgba(0, 0, 0, 0.12), 0 2px 5px 0 rgba(0, 0, 0, 0.08), 0 2px 10px 2px rgba(0, 0, 0, 0.05)",
+              "@link-color": "rgb(17, 236, 229)",
             },
             javascriptEnabled: true,
           },
