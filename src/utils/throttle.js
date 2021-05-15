d87dd2796157cc43be794a9a9063faec2ca8d310
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-15 23:27:42 +0600

    Create throttle.js

---
+++ src/utils/throttle.js
@@ -0,0 +1,13 @@
+export default function throttle(fn, delay) {
+  var timer = null;
+
+  return function () {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+      timer = null;
+    }, delay);
+  };
+}
