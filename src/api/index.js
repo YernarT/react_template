305088ced6d14c21f2672267f04e713f74887d46
Yernar <69980289+YernarT@users.noreply.github.com>
2021-05-25 23:18:14 +0600

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,24 @@
 import { get } from "@api/ajax";
 
-export const testAjax = () => get("/test-api");
+// Auth
+export const reqLog = (data) => post("auth/login/", data);
+export const reqReg = (data) =>
+  post("auth/registration/", data);
+
 
 
 // 用法:
 // 1.
 //  sendGetRequest = async () => {
-//  const responseData = await testAjax();
+//  const responseData = await reqLog({username: "xxx", password: "yyy"});
 //  console.log(responseData);
 // 
 //  console.log("request success")
 // 
 // 2.
 //  sendGetRequest = () => {
-//  testAjax().then(res => { console.log(res); }
+//  reqLog({username: "xxx", password: "yyy"})
+// .then(res => { console.log(res); })
+// .catch(err => { console.log(err) });
+
+// }
