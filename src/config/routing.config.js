d459b232c0495ca0cd022ad5bd52dcc6d9395097
[Yernar] <toktaryernar@gmail.com>
2021-08-26 22:44:49 +0600

    refactor: use const-values

--- src/config/routing.config.js
+++ src/config/routing.config.js
@@ -1,12 +1,9 @@
+import { DEBUG, ALL_AUTH, ALL_PERMISSION } from "@config";
+
 import commonRouting from "@config/routings/common-routing";
 import authRouting from "@config/routings/auth-routing";
 import userRouting from "@config/routings/user-routing";
 import adminRouting from "@config/routings/admin-routing";
-
-export const DEBUG = false;
-
-export const ALL_AUTH = [true, false];
-export const ALL_PERMISSION = ["guest", "user", "admin"];
 
 const routingConfig = [
 	...authRouting,
@@ -16,7 +13,7 @@ const routingConfig = [
 ];
 
 if (DEBUG) {
-	console.warn("Routing DEBUG!");
+	console.warn("DEBUG!");
 
 	routingConfig.forEach(conf => {
 		conf.auth = [...ALL_AUTH];
