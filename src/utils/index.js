f184af4bc7e1c128a13a8e4fb1fe6cad9f1356b2
[Yernar] <toktaryernar@gmail.com>
2021-08-27 00:19:39 +0600

    fix: remove some utils function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,3 @@
 export { default as localStorage } from "store";
 
-export { default as debounce } from "@utils/debounce";
-
-export { default as throttle } from "@utils/throttle";
-
 export { default as requireAuth } from "@utils/requireAuth";
