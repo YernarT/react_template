bc52c8e187f00b9cd67f49b96e21f3d3b5f92377
[Yernar] <toktaryernar@gmail.com>
2021-08-26 22:55:17 +0600

    fix: semantic

--- src/hooks/useBeforeunload/index.js
+++ src/hooks/useBeforeunload/index.js
@@ -4,12 +4,13 @@ import { localStorage } from "@utils";
 export default function useBeforeunload(data) {
 	useEffect(() => {
 		const handleBeforeunload = () => {
-			Object.entries(data).forEach(el => {
-				localStorage.set(el[0], el[1]);
+			Object.entries(data).forEach(([key, value]) => {
+				localStorage.set(key, value);
 			});
 		};
 
 		window.addEventListener("beforeunload", handleBeforeunload);
+
 		return () => {
 			window.removeEventListener("beforeunload", handleBeforeunload);
 		};
