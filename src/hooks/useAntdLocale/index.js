0595bad5cbe0738943bf36de56b6ed0da10fc4b0
[Yernar] <toktaryernar@gmail.com>
2021-09-10 23:26:28 +0600

    fix: 默认为英语, 跳过翻译阶段

--- src/hooks/useAntdLocale/index.js
+++ src/hooks/useAntdLocale/index.js
@@ -1,7 +1,5 @@
 import en_US from "antd/lib/locale/en_US";
 import zh_CN from "antd/lib/locale/zh_CN";
-import ru_RU from "antd/lib/locale/ru_RU";
-import kk_KZ from "antd/lib/locale/kk_KZ";
 
 export default function useAntdLocale(locale) {
 	let antdLocale;
@@ -12,12 +10,6 @@ export default function useAntdLocale(locale) {
 			break;
 		case "zh-CN":
 			antdLocale = zh_CN;
-			break;
-		case "ru-RU":
-			antdLocale = ru_RU;
-			break;
-		case "kk-KZ":
-			antdLocale = kk_KZ;
 			break;
 		default:
 			antdLocale = en_US;
--- src/i18n/messages/index.js
+++ src/i18n/messages/index.js
@@ -1,13 +1,7 @@
-import i18nENGLISH from "@i18n/messages/en-US";
 import i18nCHINESE from "@i18n/messages/zh-CN";
-import i18nRUSSIAN from "@i18n/messages/ru-RU";
-import i18nKAZAH from "@i18n/messages/kk-KZ";
 
 const i18nLanguages = {
-	...i18nENGLISH,
 	...i18nCHINESE,
-	...i18nRUSSIAN,
-	...i18nKAZAH,
 };
 
 export default i18nLanguages;
