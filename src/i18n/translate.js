fdef618adf79ebb24e06a235770432dcc0a5650c
[Yernar] <toktaryernar@gmail.com>
2021-09-10 23:30:15 +0600

    feat: 翻译支持渲染标签, 以及翻译时的约定

--- src/i18n/translate.js
+++ src/i18n/translate.js
@@ -3,16 +3,47 @@ import i18nLanguages from "@i18n/messages";
 import { useRecoilValue } from "recoil";
 import { pageAtom } from "@recoil";
 
-/**
- * Todo:
- * 1) Lazy loading language pack
- * 2) Regular replacement, values replace {variable}
- */
-
-const translate = (id, values = {}) => {
+const translate = (string, config = null) => {
 	const { locale } = useRecoilValue(pageAtom);
 
-	return i18nLanguages[locale][id];
+	if (config) {
+		/**
+		 * 约定翻译时, 不翻译将要替换的string
+		 */
+
+		// 默认语言为英语, 不翻译
+		if (locale !== "en-US") {
+			string = i18nLanguages[locale][string];
+		} 
+
+		config.innerWrap.forEach(el => {
+			let attr = "";
+			el.attr.forEach(el => {
+				attr += `${el.name}="${el.value}"`;
+			});
+
+			string = string.replaceAll(
+				el.string,
+				`<${el.wrap} ${attr}}>${el.string}</${el.wrap}>`,
+			);
+		});
+
+		let attr = "";
+		config.attr.forEach(el => {
+			attr += `${el.name}="${el.value}"`;
+		});
+
+		string = `<${config.wrap} ${attr}>${string}</${config.wrap}>`;
+
+		return string;
+	} else {
+		// 默认语言为英语, 不翻译
+		if (locale === "en-US") {
+			return string;
+		}
+
+		return i18nLanguages[locale][string];
+	}
 };
 
 export default translate;
