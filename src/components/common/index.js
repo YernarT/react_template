c4aa5378b6e4c5a3f9b2f73a5c75cc8311f3d450
YernarT <toktaryernar@gmail.com>
2021-11-29 09:51:14 +0600

    update: 更新组件导出

---
+++ src/components/common/index.js
@@ -0,0 +1,13 @@
+/**
+ * comprehensive版本 核心组件之一
+ *
+ * 此处只是一个栗子 🌰
+ * 根据项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ */
+
+export { default as RouteWithConfig } from '@components/common/RouteWithConfig';
+
+export { default as Loading } from '@components/common/Loading';
+
+export { default as LayoutTemplate } from '@components/common/LayoutTemplate';
--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-// Common Components ↓
-export { default as RouteWithConfig } from "@components/common/RouteWithConfig";
-
-// Admin Components ↓
-
-// User Components ↓
