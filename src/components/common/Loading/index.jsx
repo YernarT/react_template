a7f3829e95d60178bd55670cc88e9be41b9408d4
YernarT <toktaryernar@gmail.com>
2021-11-29 09:54:42 +0600

    feat: Loading组件 案例

---
+++ src/components/common/Loading/index.jsx
@@ -0,0 +1,10 @@
+// 一个简单的案例
+// 根据项目需求自行修改, 删除
+
+import React from 'react';
+
+import { LoadingContainer } from './styles';
+
+export default function Loading() {
+	return <LoadingContainer>加载中...</LoadingContainer>;
+}
