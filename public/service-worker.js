4f678139e6900a8111cfbc1f0d478159f041ca10
YernarT <toktaryernar@gmail.com>
2021-11-29 09:38:50 +0600

    feat: 基本的 service worker 脚本

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+/**
+ * Service Worker 脚本文件
+ * 编写 PWA 相关逻辑
+ *  */
+
+// Insatll Service Worker
+self.addEventListener('install', () => {});
+
+// Listen for requests
+self.addEventListener('fetch', () => {});
+
+// Activate the Service Worker
+self.addEventListener('activate', () => {});
