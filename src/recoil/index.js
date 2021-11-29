f2fb9584b3c72485feb9800dbc1f58ee6d072040
YernarT <toktaryernar@gmail.com>
2021-11-29 09:48:09 +0600

    update: 添加注释

--- src/recoil/index.js
+++ src/recoil/index.js
@@ -1,2 +1,11 @@
-export { userAtom } from "@recoil/user-atom";
-export { pageAtom } from "@recoil/page-atom";
+/**
+ * recoil库 所使用的 atom
+ * 
+ * 此处只是一个栗子 🌰
+ * 根据项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ */
+
+export { userAtom } from '@recoil/user-atom';
+
+export { pageAtom } from '@recoil/page-atom';
--- src/recoil/user-atom.js
+++ src/recoil/user-atom.js
@@ -1,14 +1,25 @@
-import { atom } from "recoil";
-import { localStorage } from "@utils";
+/**
+ * 用户相关的 全局状态
+ *
+ * 此处只是一个栗子 🌰
+ * 根据项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ */
+import { atom } from 'recoil';
+import { localStorage } from '@utils';
 
 export const userAtom = atom({
-	key: "userAtom",
-	default: localStorage.get("user", {
+	key: 'userAtom',
+	// 默认值
+	default: localStorage.get('user', {
+		// 用户 ID
 		id: null,
-		username: "",
-		email: "",
-		userType: "guest",
-		jwt: "",
-		refreshJWT: "",
+		// 用户名
+		username: '',
+		// 角色
+		role: 'guest',
+		// JWT
+		jwt: '',
+		refreshJWT: '',
 	}),
 });
