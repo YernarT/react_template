84300eeb22a5e4fc4c635e1fcf077f4b89b6c397
YernarT <toktaryernar@gmail.com>
2021-11-29 09:51:37 +0600

    update: 添加注释

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
 
-import { RecoilRoot } from "recoil";
+import { RecoilRoot } from 'recoil';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
+	// 严格模式 检测错误
 	<StrictMode>
+		{/* Recoil根组件 推荐放在这里 内部使用React Context API 双向数据流 */}
 		<RecoilRoot>
 			<App />
 		</RecoilRoot>
 	</StrictMode>,
-	document.getElementById("root"),
+
+	document.getElementById('root'),
 );
