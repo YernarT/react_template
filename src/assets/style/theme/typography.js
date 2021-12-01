f68a9b16f31f4f9e3b6c6c705047af72eab5a753
YernarT <toktaryernar@gmail.com>
2021-12-01 14:44:44 +0600

    update: 更新双模式颜色

--- src/assets/style/theme/typography.js
+++ src/assets/style/theme/typography.js
@@ -1,25 +1,53 @@
 /**
  * typography 相关样式
  * viewMode 为分辨 light, dark模式, 两种不同样式
- * 
+ *
  * 此处只是一个案例, 根据实际的项目需求自行修
  * 有更好的意见可以提 issue, pr
  * Github地址: https://github.com/YernarT/quick-start-for-react
-*/
+ */
 
 export default {
 	// 可以根据 h1, h2, h3... p, span 这样去设计
 
-	title: {
-		fontWeight: 800,
-		fontSize: '1.62rem',
-		color: 'rgba(0, 0, 0, 0.85)',
+	title: viewMode => {
+		if (viewMode === 'dark') {
+			return {
+				fontWeight: 700,
+				fontSize: '1.14rem',
+				color: 'rgba(255, 255, 255, 0.85)',
+
+				marginBottom: 0,
+			};
+		} else {
+			return {
+				fontWeight: 700,
+				fontSize: '1.14rem',
+				color: 'rgba(0, 0, 0, 0.85)',
+
+				marginBottom: 0,
+			};
+		}
 	},
 
-	text: {
-		fontWeight: 500,
-		fontSize: '1.12rem',
-		color: '#5f6367',
+	text: viewMode => {
+		if (viewMode === 'dark') {
+			return {
+				fontWeight: 600,
+				fontSize: '0.86rem',
+				color: 'rgba(255, 255, 255, 0.85)',
+
+				marginBottom: 0,
+			};
+		} else {
+			return {
+				fontWeight: 600,
+				fontSize: '0.86rem',
+				color: 'rgba(0, 0, 0, 0.85)',
+
+				marginBottom: 0,
+			};
+		}
 	},
 
 	link: viewMode => {
