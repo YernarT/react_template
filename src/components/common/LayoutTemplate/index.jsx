3b1d7d468c16130eb5a459457022c61380d1adf3
YernarT <toktaryernar@gmail.com>
2021-12-01 23:57:21 +0600

    update: LayoutTemplate 样式升级

--- src/components/common/LayoutTemplate/index.jsx
+++ src/components/common/LayoutTemplate/index.jsx
@@ -9,10 +9,18 @@ import React from 'react';
 import { useRecoilState } from 'recoil';
 import { pageAtom } from '@recoil';
 
-import { LayoutTemplateContainer, NavbarContainer } from './styles';
+import { use1vh } from 'src/hooks';
+
+import {
+	LayoutTemplateContainer,
+	NavbarContainer,
+	Content,
+	FooterContainer,
+} from './styles';
 
 export default function LayoutTemplate({ children }) {
 	const [page, setPage] = useRecoilState(pageAtom);
+	const oneVh = use1vh();
 
 	// 切换显示模式, 简单的案例
 	// 根据项目需求自行修改
@@ -21,29 +29,45 @@ export default function LayoutTemplate({ children }) {
 	};
 
 	return (
-		<LayoutTemplateContainer viewMode={page.viewMode}>
+		<LayoutTemplateContainer viewMode={page.viewMode} oneVh={oneVh}>
 			<Navbar viewMode={page.viewMode} changeViewMode={handleChangeViewMode} />
-			{children}
+
+			<Content viewMode={page.viewMode}>{children}</Content>
+			<Footer viewMode={page.viewMode} />
 		</LayoutTemplateContainer>
 	);
 }
 
-//
+// 此处只是一个案例, 真实开发可以拆分文件编写
 function Navbar({ viewMode, changeViewMode }) {
 	return (
-		<NavbarContainer viewMode={viewMode}>
-			<h1>Navbar</h1>
-			<p
-				className="change-view-mode"
-				onClick={() => {
-					if (viewMode === 'dark') {
-						changeViewMode('light');
-					} else {
-						changeViewMode('dark');
-					}
-				}}>
-				点击切换 view mode
-			</p>
-		</NavbarContainer>
+		<>
+			<NavbarContainer viewMode={viewMode}>
+				<h1 className="title">Quick start for React | comprehensive</h1>
+
+				{/* <p
+					className="change-view-mode"
+					onClick={() => {
+						if (viewMode === 'dark') {
+							changeViewMode('light');
+						} else {
+							changeViewMode('dark');
+						}
+					}}>
+					点击切换 view mode
+				</p> */}
+			</NavbarContainer>
+		</>
+	);
+}
+
+// 此处只是一个案例, 真实开发可以拆分文件编写
+function Footer({ viewMode }) {
+	return (
+		<FooterContainer viewMode={viewMode}>
+			<h2 className="title">
+				Quick start for React ©2021 Created by IT IS IT Studio
+			</h2>
+		</FooterContainer>
 	);
 }
--- src/components/common/LayoutTemplate/styles.js
+++ src/components/common/LayoutTemplate/styles.js
@@ -4,42 +4,132 @@
  * Github地址: https://github.com/YernarT/quick-start-for-react
  */
 
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+/**
+ *
+ * @param {'margin'|'padding'} spaceType
+ * 空间类型
+ * 
+ * @returns {Object}
+ * 返回响应式空间对象
+ */
+const responsiveSpace = spaceType => css`
+	@media screen and (min-width: 1230px) {
+		${spaceType === 'margin'
+			? `
+			margin: 30px 15%;
+			margin-bottom: 0;`
+			: `
+			padding: 30px 15%;
+			padding: 0;`}
+	}
+	@media screen and (min-width: 920px) {
+		${spaceType === 'margin'
+			? `
+			margin: 26px 12%;
+			margin-bottom: 0;`
+			: `
+			padding: 26px 12%;
+			padding: 0;`}
+	}
+	@media screen and (min-width: 585px) {
+		${spaceType === 'margin'
+			? `
+			margin: 22px 8%;
+			margin-bottom: 0;`
+			: `
+			padding: 22px 8%;
+			padding: 0;`}
+	}
+	@media screen and (max-width: 585px) {
+		${spaceType === 'margin'
+			? `
+			margin: 18px 6%;
+			margin-bottom: 0;`
+			: `
+			padding: 18px 6%;
+			padding: 0;`}
+	}
+`;
 
 export const LayoutTemplateContainer = styled.div`
 	width: 100%;
-	height: auto;
+	// 解决移动端浏览器无法正常显示100vh的问题
+	height: ${({ oneVh }) => {
+		if (oneVh) {
+			return `${100 * oneVh}px`;
+		} else {
+			return '100vh';
+		}
+	}};
 
 	// 对于使用此 LayoutTemplate组件 的页面 形成一个外壳
 	// 使其相对于 LayoutTemplate 进行定位
 	// 根据项目需求自行修改
 	position: relative;
+
+	overflow: hidden;
+
+	// assets 目录中 style -> theme 对象
+	// 通过App.jsx 中 StyledThemeProviderd 获取
+	background: ${({ viewMode }) =>
+		viewMode === 'dark'
+			? 'linear-gradient(90deg, #141e30, #243b55)'
+			: 'linear-gradient(90deg, #2daee9, #509bf0)'};
+
+	display: grid;
+	grid-template-rows: 64px 1fr 46px;
+	grid-template-columns: 1fr;
 `;
 
 export const NavbarContainer = styled.div`
-	width: 100%;
-	height: 46px;
-
-	position: fixed;
-	top: 0;
-	left: 0;
-	// assets 目录中 style -> theme 对象
-	// 通过App.jsx 中 StyledThemeProviderd 获取
-	z-index: ${({ theme }) => theme.zIndex.navbar};
-
-	background-color: ${({ viewMode }) =>
-		viewMode === 'dark' ? 'rgba(255, 255, 255, 0.64)' : 'rgba(0, 0, 0, 0.64)'};
-	border-bottom: 2px solid
-		${({ viewMode }) =>
-			viewMode === 'dark' ? 'rgba(255, 255, 255, 1)' : 'rgba(0, 0, 0, 1)'};
+	box-shadow: 0 2px 16px
+		${({ viewMode }) => (viewMode === 'dark' ? '#004eff' : '#007ad1')};
 
 	display: flex;
 	justify-content: center;
 	align-items: center;
 
-	.change-view-mode {
-		margin-left: auto;
+	${responsiveSpace('padding')}
 
-		cursor: pointer;
+	transition: margin 0.3s ease;
+
+	.title {
+		${({ viewMode, theme }) => theme.typography.title(viewMode)};
+	}
+
+	.change-view-mode {
+		/* margin-left: auto;
+
+		cursor: pointer; */
+	}
+`;
+
+export const Content = styled.main`
+	background-color: rgba(255, 255, 255, 0.14);
+
+	${responsiveSpace('margin')}
+
+	transition: margin 0.3s ease;
+`;
+
+export const FooterContainer = styled.footer`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	padding: 0 5px;
+
+	overflow: hidden;
+
+	.title {
+		${({ viewMode, theme }) => theme.typography.title(viewMode)};
+		font-size: 0.89rem;
+		font-weight: ${({ viewMode, theme }) =>
+			theme.typography.text(viewMode).fontWeight};
+
+		text-overflow: ellipsis;
+		overflow: hidden;
 	}
 `;
