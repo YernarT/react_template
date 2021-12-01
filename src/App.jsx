48f71362d0275ef64fe5218a3a20fe658941c191
YernarT <toktaryernar@gmail.com>
2021-12-01 11:03:24 +0600

    docs: 丰富注释内容

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
+/**
+ * 此处只是一个案例, 根据实际的项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
+ */
+
+/**
+ * 引入顺序规则如下 (仅推荐)
+ * 1. React相关库 以React核心库为首
+ * 2. React周边库 比如 Redux, Recoil等
+ * 3. 第三方组件库 比如 MaterialUI, Antd等
+ * 4. 自己封装的组件 components目录
+ * 5. 第三方Hook库 比如 react-hooks, ahooks
+ * 6. 自己封装的Hook hooks目录
+ * 7. API 函数
+ * 8. Util 工具函数, config配置 (若导入过多, 可分层)
+ * 9. 资源文件, 比如 assets目录
+ * 10. 其他
+ *
+ * 不同层级(序号)之间用空行分隔
+ */
 import React, { useCallback, Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -14,6 +35,7 @@ import { RouteWithConfig, Loading } from '@components/common';
 
 import { useEventListener } from 'ahooks';
 import { useAntdLocale } from '@hooks';
+
 import { localStorage } from '@utils';
 import { routingConfig } from '@config';
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * 此处只是一个案例, 根据实际的项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
+ */
+
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 
@@ -8,7 +14,7 @@ import App from './App';
 ReactDOM.render(
 	// 严格模式 检测错误
 	<StrictMode>
-		{/* Recoil根组件 推荐放在这里 内部使用React Context API 双向数据流 */}
+		{/* Recoil 根组件 推荐放在这里 内部使用 React Context API 双向数据流 */}
 		<RecoilRoot>
 			<App />
 		</RecoilRoot>
