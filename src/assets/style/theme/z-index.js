70535c7f8a7014da0c4fb9055287878b2dd80371
YernarT <toktaryernar@gmail.com>
2021-12-01 14:41:26 +0600

    fix: 删除navbar z-index属性

--- src/assets/style/theme/z-index.js
+++ src/assets/style/theme/z-index.js
@@ -1,17 +1,14 @@
 /**
  * z-index 相关样式
- * 
+ *
  * 此处只是一个案例, 根据实际的项目需求自行修
  * 有更好的意见可以提 issue, pr
  * Github地址: https://github.com/YernarT/quick-start-for-react
-*/
+ */
 
 export default {
 	// antd 组件中使用到 absolute或fixed定位元素的 z-index
 	modal: 1000,
 	message: 1050,
 	tooltip: 1100,
-
-	// components 目录中 LayoutTemplate 案例里的 Navbar 组件采用 fixed 布局固顶
-	navbar: 100,
 };
