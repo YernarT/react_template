dac114480270c5751f361a04cef0235ca9f6515e
YernarT <toktaryernar@gmail.com>
2021-12-01 11:17:05 +0600

    docx: 丰富注释内容

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,11 @@
 /**
  * Service Worker 脚本文件
  * 编写 PWA 相关逻辑
- *  */
+ *
+ * 此处只是一个案例, 根据实际的项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
+ * */
 
 // Insatll Service Worker
 self.addEventListener('install', () => {});
