0afd206c10beec067a65f967f2d395c36d375bc6
YernarT <toktaryernar@gmail.com>
2021-12-01 11:14:01 +0600

    docx: 丰富注释内容

--- src/recoil/page-atom.js
+++ src/recoil/page-atom.js
@@ -1,9 +1,9 @@
 /**
  * 页面相关的 全局状态
  *
- * 此处只是一个栗子 🌰
- * 根据项目需求自行修改
+ * 此处只是一个案例, 根据实际的项目需求自行修改
  * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
  */
 
 import { atom } from 'recoil';
--- src/recoil/user-atom.js
+++ src/recoil/user-atom.js
@@ -1,9 +1,9 @@
 /**
  * 用户相关的 全局状态
  *
- * 此处只是一个栗子 🌰
- * 根据项目需求自行修改
+ * 此处只是一个案例, 根据实际的项目需求自行修改
  * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
  */
 import { atom } from 'recoil';
 import { localStorage } from '@utils';
