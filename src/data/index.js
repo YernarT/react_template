9171785550324fef6cf5c41488478fbea88cf22a
YernarT <toktaryernar@gmail.com>
2021-12-01 11:25:03 +0600

    docx: 更完整注释

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,10 @@
-// data 相关文件
-// 根据项目需求自行修改
+/**
+ * 统一导出 data 文件, 方便使用
+ * data目录 储存一些静态信息, 在多处引用 或 以后会经常变更
+ *
+ * 此处只是一个案例, 根据实际的项目需求自行修改
+ * 有更好的意见可以提 issue, pr
+ * Github地址: https://github.com/YernarT/quick-start-for-react
+ */
 
 export { default as constantData } from './constant-data.json';
