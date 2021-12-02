ccf3c6985aaa51c7f8b0421af9157902f02e6014
YernarT <toktaryernar@gmail.com>
2021-12-02 16:28:38 +0600

    update: 添加案例

--- src/i18n/messages/zh-CN.js
+++ src/i18n/messages/zh-CN.js
@@ -12,6 +12,9 @@ import i18nLocales from '@i18n/locales';
 const i18nCHINESE = {
 	[i18nLocales.CHINESE.code]: {
 		'Sorry, the page you visited does not exist.': '抱歉，您访问的页面不存在。',
+		'Back Home': '回到主页',
+		'Loading...': '加载中...',
+		'Created by IT IS IT Studio': '由 IT IS IT 工作室制作',
 	},
 };
 
