a7907520e41cf666c8a93cc9f549f9bd11866590
YernarT <toktaryernar@gmail.com>
2021-12-02 15:50:29 +0600

    fix: 使用 useRecoilValue 替代 State

--- src/components/common/LayoutTemplate/index.jsx
+++ src/components/common/LayoutTemplate/index.jsx
@@ -6,7 +6,7 @@
 
 import React from 'react';
 
-import { useRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import { pageAtom } from '@recoil';
 
 import { use1vh } from 'src/hooks';
@@ -19,18 +19,12 @@ import {
 } from './styles';
 
 export default function LayoutTemplate({ children }) {
-	const [page, setPage] = useRecoilState(pageAtom);
+	const page = useRecoilValue(pageAtom);
 	const oneVh = use1vh();
-
-	// 切换显示模式, 简单的案例
-	// 根据项目需求自行修改
-	const handleChangeViewMode = viewMode => {
-		setPage(Object.assign({}, page, { viewMode }));
-	};
 
 	return (
 		<LayoutTemplateContainer viewMode={page.viewMode} oneVh={oneVh}>
-			<Navbar viewMode={page.viewMode} changeViewMode={handleChangeViewMode} />
+			<Navbar viewMode={page.viewMode} />
 
 			<Content viewMode={page.viewMode}>{children}</Content>
 			<Footer viewMode={page.viewMode} />
@@ -39,7 +33,7 @@ export default function LayoutTemplate({ children }) {
 }
 
 // 此处只是一个案例, 真实开发可以拆分文件编写
-function Navbar({ viewMode, changeViewMode }) {
+function Navbar({ viewMode }) {
 	return (
 		<>
 			<NavbarContainer viewMode={viewMode}>
