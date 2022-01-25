e824e310ce239b66dcc3aa9415cfb60660d3b214
YernarT <toktaryernar@gmail.com>
2022-01-25 18:14:28 +0600

    feat: ✨添加 prettier 规范

---
+++ .prettierrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	// 单行可显示的最大字符串长度
+	printWidth: 80,
+	// 缩进字节数
+	tabWidth: 2,
+	// 使用Tab
+	useTabs: true,
+	// 句尾添加分号
+	semi: true,
+	// 使用单引号
+	singleQuote: true,
+	quoteProps: 'preserve',
+	// jsx 使用单引号
+	jsxSingleQuote: false,
+	// 在对象或数组最后一个元素后面是否加逗号
+	trailingComma: 'all',
+	// 在对象或数组括号于文字之间添加空格
+	bracketSpacing: true,
+	// 在jsx中把 '>' 是否单独放一行
+	jsxBracketSameLine: true,
+	// (x) => {} 箭头函数参数只有一个时是否要有小括号 avoid: 省略括号
+	arrowParens: 'avoid',
+	rangeStart: 0,
+	rangeEnd: Infinity,
+	requirePragma: false,
+	insertPragma: false,
+	proseWrap: 'preserve',
+	htmlWhitespaceSensitivity: 'css',
+	// 结尾是 \n \r \n\r auto
+	endOfLine: 'auto',
+};
