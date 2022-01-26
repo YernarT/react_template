778a9230103f8775bbd764a57eca23dbb41915cc
YernarT <toktaryernar@gmail.com>
2022-01-26 20:12:42 +0600

    feat: ✨配置 alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@config': path.resolve(__dirname, 'src/config'),
+			'@layout': path.resolve(__dirname, 'src/layout'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@routes': path.resolve(__dirname, 'src/routes'),
+		},
+	},
 	plugins: [react()],
 });
