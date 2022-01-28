3fac826e87a0618593ebf8439f6df4addbd01f79
YernarT <toktaryernar@gmail.com>
2022-01-28 12:00:49 +0600

    update: :hammer: alias 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { resolve } from 'path';
+
+function pathResolve(dir: string): string {
+	return resolve(process.cwd(), '.', dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
-		alias: {
-			'@assets': path.resolve(__dirname, 'src/assets'),
-			'@components': path.resolve(__dirname, 'src/components'),
-			'@config': path.resolve(__dirname, 'src/config'),
-			'@layout': path.resolve(__dirname, 'src/layout'),
-			'@pages': path.resolve(__dirname, 'src/pages'),
-			'@routes': path.resolve(__dirname, 'src/routes'),
-		},
+		alias: [
+			{
+				find: '@',
+				replacement: pathResolve('src') + '/',
+			},
+			{
+				find: '#',
+				replacement: pathResolve('types') + '/',
+			},
+		],
 	},
 	plugins: [react()],
 });
