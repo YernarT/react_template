f3a38bffe91e0e72ef07c4d02845fdb665505cb3
YernarT <toktaryernar@gmail.com>
2022-02-09 23:34:57 +0600

    wip: :construction: mui 实践

--- src/pages/user/HomePage/index.tsx
+++ src/pages/user/HomePage/index.tsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+
+import { TextField } from '@mui/material';
+import { DatePicker } from '@mui/lab';
 import { Count } from '@/components/user';
 
 export default function HomePage() {
+	const [value, setValue] = useState('09-02-2002');
 	return (
 		<div className="home-page">
 			<h1>Home Page</h1>
 
 			<Count />
+			<hr />
+
+			<DatePicker
+				label="Basic example"
+				mask='__.__.____'
+				value={value}
+				onChange={newValue => {
+					setValue(newValue || '');
+				}}
+				renderInput={params => <TextField {...params} />}
+			/>
 		</div>
 	);
 }
