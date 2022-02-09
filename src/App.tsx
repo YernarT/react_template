43aba1fd58ffe73cb23f72ecd8f79b2126258b8e
YernarT <toktaryernar@gmail.com>
2022-02-09 23:34:50 +0600

    wip: :construction: mui 实践

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
+import { CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { kzKZ } from '@mui/material/locale';
+import { LocalizationProvider } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import { kk } from 'date-fns/locale';
 
 import { RouteGuard } from '@/components/common';
 import routes from '@/routes';
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#f42160',
+const theme = createTheme(
+	{
+		palette: {
+			mode: 'dark',
 		},
 	},
-});
+	kzKZ,
+);
 
 function App() {
 	return (
 		<RecoilRoot>
 			<BrowserRouter>
 				<ThemeProvider theme={theme}>
-					<RouteGuard routes={routes} />
+					<LocalizationProvider dateAdapter={AdapterDateFns} locale={kk}>
+						<CssBaseline>
+							<RouteGuard routes={routes} />
+						</CssBaseline>
+					</LocalizationProvider>
 				</ThemeProvider>
 			</BrowserRouter>
 		</RecoilRoot>
