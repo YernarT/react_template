1ae99583d796c2ca3b1debc06a7489d65688f95d
YernarT <toktaryernar@gmail.com>
2022-02-10 23:45:08 +0600

    feat: :sparkles: 新增工具函数, 检测本地数据是否"过期"

---
+++ src/utils/getLatestState.ts
@@ -0,0 +1,25 @@
+/**
+ * Check the contents of localStorage are out of date or incorrect
+ */
+
+import type { isObject } from '#/common';
+
+export default function getLatestState<T extends isObject>(
+	dataToBeDetected: T,
+	defaultCorrectData: T,
+): [boolean, T] {
+	let fromStorageKeys = Object.keys(dataToBeDetected);
+	let defaultKeys = Object.keys(defaultCorrectData);
+
+	if (fromStorageKeys.length !== defaultKeys.length) {
+		return [false, defaultCorrectData];
+	}
+
+	fromStorageKeys.forEach(key => {
+		if (!defaultKeys.includes(key)) {
+			return [false, defaultCorrectData];
+		}
+	});
+
+	return [true, dataToBeDetected];
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ export type { RoleProperties } from './role';
 
 export { localStorage } from './localStorage';
 export type { localStorageProperties } from './localStorage';
+
+export { default as getLatestState } from './getLatestState';
