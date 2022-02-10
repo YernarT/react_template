11ede658bc6d54371f4648d2af553cd2188a3a1c
YernarT <toktaryernar@gmail.com>
2022-02-10 18:08:51 +0600

    feat: :sparkles: localStorage工具函数

---
+++ src/utils/localStorage.ts
@@ -0,0 +1,18 @@
+import type { isObject } from '#/common';
+
+export interface localStorageProperties {
+	get(key: string, defaultValue: isObject): isObject;
+	set(key: string, value: isObject): void;
+}
+
+export const localStorage: localStorageProperties = {
+	get(key, defaultValue) {
+		return JSON.parse(
+			window.localStorage.getItem(key) || JSON.stringify(defaultValue),
+		);
+	},
+
+	set(key, value) {
+		window.localStorage.setItem(key, JSON.stringify(value));
+	},
+};
