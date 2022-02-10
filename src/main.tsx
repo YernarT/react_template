ac6ec798ecebc760688f4911b420c2d23129c3cd
YernarT <toktaryernar@gmail.com>
2022-02-10 23:53:55 +0600

    update: :hammer: 将 RecoilRoot提升至 main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 
 import App from './App';
 
 ReactDOM.render(
 	<StrictMode>
-		<App />
+		<RecoilRoot>
+			<App />
+		</RecoilRoot>
 	</StrictMode>,
 	document.getElementById('root'),
 );
