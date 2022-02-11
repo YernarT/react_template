d5844ee37c1d3498ecc91ed84a0170e12ae037a2
YernarT <toktaryernar@gmail.com>
2022-02-11 13:41:08 +0600

    feat: :sparkles: 支持本地化状态恢复, theme 对象动态化

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,48 @@
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import { useRecoilValue } from 'recoil';
+import { userAtom, pageAtom } from '@/store';
 
 import { CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { kzKZ } from '@mui/material/locale';
+import { ThemeProvider } from '@mui/material/styles';
 import { LocalizationProvider } from '@mui/lab';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { kk } from 'date-fns/locale';
 
 import { RouteGuard } from '@/components/common';
 import routes from '@/routes';
 
-const theme = createTheme(
-	{
-		palette: {
-			mode: 'dark',
-		},
-	},
-	kzKZ,
-);
+import { useEventListener, useCreation } from 'ahooks';
+import { localStorage, getLocale } from '@/utils';
+import getTheme from '@/assets/theme';
 
 function App() {
+	// 全局状态
+	const user = useRecoilValue(userAtom);
+	const page = useRecoilValue(pageAtom);
+
+	// 刷新页面 保存全局状态到本地, 用于下次打开页面时恢复
+	useEventListener('beforeunload', () => {
+		localStorage.set('user', user);
+		localStorage.set('page', page);
+	});
+
+	const locale = useCreation(() => getLocale(page.locale), [page.locale]);
+	const theme = useCreation(
+		() => getTheme(locale.uiLocale, page.viewMode),
+		[page],
+	);
+
 	return (
-		<RecoilRoot>
-			<BrowserRouter>
-				<ThemeProvider theme={theme}>
-					<LocalizationProvider dateAdapter={AdapterDateFns} locale={kk}>
-						<CssBaseline>
-							<RouteGuard routes={routes} />
-						</CssBaseline>
-					</LocalizationProvider>
-				</ThemeProvider>
-			</BrowserRouter>
-		</RecoilRoot>
+		<BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<LocalizationProvider
+					dateAdapter={AdapterDateFns}
+					locale={locale.dateLocale}>
+					<CssBaseline>
+						<RouteGuard routes={routes} />
+					</CssBaseline>
+				</LocalizationProvider>
+			</ThemeProvider>
+		</BrowserRouter>
 	);
 }
 
