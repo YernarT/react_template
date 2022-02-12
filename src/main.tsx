0d8fa4893a048edd0fb2f94a5cae9b753ff5a72a
YernarT <toktaryernar@gmail.com>
2022-02-13 00:46:00 +0600

    feat: :sparkles: 初始化 i18n

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,40 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import { enUS, kkKZ, zhCN } from '@/i18n';
+
+import { getLatestState, localStorage } from '@/utils';
+import { defaultPageState } from '@/store';
+
 import App from './App';
+
+// 初始化语言, 从 LocalStorage 获取
+const [_, state] = getLatestState(
+	localStorage.get('page', {}),
+	defaultPageState,
+);
+
+i18next.use(initReactI18next).init({
+	resources: {
+		enUS: {
+			translation: enUS,
+		},
+		kkKZ: {
+			translation: kkKZ,
+		},
+		zhCN: {
+			translation: zhCN,
+		},
+	},
+	lng: state.locale,
+	// 兜底语言
+	fallbackLng: 'kkKZ',
+	interpolation: {
+		escapeValue: false,
+	},
+});
 
 ReactDOM.render(
 	<StrictMode>
