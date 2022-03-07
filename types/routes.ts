a7de95c67acbf0fdcb2645a4ebbddd272b94e5cc
YernarT <toktaryernar@gmail.com>
2022-03-07 17:37:10 +0600

    feat: :sparkles: add role type, title attr

--- types/routes.ts
+++ types/routes.ts
@@ -1,9 +1,12 @@
 import type { LazyExoticComponent } from 'react';
 
+export type allowdRole = 'guest' | 'user' | 'admin';
+
 export interface RouteProps {
 	path: string | string[];
 	component: LazyExoticComponent<() => JSX.Element>;
 	fallback: JSX.Element;
 	auth: boolean;
-	role: string[];
+	role: allowdRole[];
+	title?: string;
 }
