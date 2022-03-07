28ff61a03041c25ec5fad5393c7da9d1f7de1e33
YernarT <toktaryernar@gmail.com>
2022-03-07 17:36:18 +0600

    update: :hammer: add simple business logic

--- src/components/common/RouteGuard/index.tsx
+++ src/components/common/RouteGuard/index.tsx
@@ -3,6 +3,10 @@ import type { RouteProps } from '#/routes';
 import { memo, Suspense } from 'react';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 
+import { useTitle } from 'ahooks';
+import { useRecoilValue } from 'recoil';
+import { userAtom } from '@/store';
+
 interface RouteGuardProps {
 	routes: RouteProps[];
 }
@@ -10,21 +14,42 @@ interface RouteGuardProps {
 export default memo(function RouteGuard({ routes }: RouteGuardProps) {
 	const { pathname } = useLocation();
 
-	const targetConfig = routes.find(
-		routeConfig => routeConfig.path === pathname,
-	);
+	const user = useRecoilValue(userAtom);
 
-	if (targetConfig) {
-		if (targetConfig.auth && targetConfig.role) {
-			// some logic
+	const targetConfig = routes.find(routeConfig => {
+		if (Array.isArray(routeConfig.path)) {
+			return routeConfig.path.includes(pathname);
 		}
 
+		return routeConfig.path === pathname;
+	});
+
+	useTitle(targetConfig?.title || document.title);
+
+	// Registered route
+	if (targetConfig) {
+		// Require authorization, but user not authorized
+		if (targetConfig.auth && Boolean(user.jwt) === false) {
+			return <Redirect to="/auth/login" />;
+		}
+
+		// Disallowed user role
+		if (!targetConfig.role.includes(user.role)) {
+			return <Redirect to="/404" />;
+		}
+
+		// Everything is ok
 		return (
 			<Suspense fallback={targetConfig.fallback}>
-				<Route path={targetConfig.path} component={targetConfig.component} />;
+				<Route
+					exact
+					path={targetConfig.path}
+					component={targetConfig.component}
+				/>
 			</Suspense>
 		);
 	}
 
+	// Unregistered route
 	return <Redirect to="/404" />;
 });
