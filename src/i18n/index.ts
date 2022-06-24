3e7f90d3ccb09336ccc3bc7d25d4d0843bfd81ea
YernarT <toktaryernar@gmail.com>
2022-06-24 21:18:43 +0600

    update: :hammer: 新增 allowedLocale

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,3 +3,5 @@ export { default as enUS } from './enUS';
 export { default as kkKZ } from './kkKZ';
 
 export { default as zhCN } from './zhCN';
+
+export type allowedLocale = 'enUS' | 'kkKZ' | 'zhCN';
