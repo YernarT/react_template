f732cfc53d1e4b777c824028d9d15ddf36495a9d
YernarT <toktaryernar@gmail.com>
2022-06-24 21:27:52 +0600

    update: :hammer: 添加注释

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
+// 类型
 import type { pageStateProperties } from '@/store';
 
+// React & 周边库
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { RecoilRoot } from 'recoil';
 
+// 业务库
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import intervalPlural from 'i18next-intervalplural-postprocessor';
 import { enUS, kkKZ, zhCN } from '@/i18n';
 
+// 工具函数
 import { getLatestState, localStorage, getHtmlLang } from '@/utils';
 import { defaultPageState } from '@/store';
 
+// 组件
 import { SafeArea } from './components/common';
 import App from './App';
 
@@ -21,6 +26,7 @@ const [_, page] = getLatestState(
 	defaultPageState,
 );
 
+// 初始化 i18n模块
 i18next
 	.use(initReactI18next)
 	.use(intervalPlural)
